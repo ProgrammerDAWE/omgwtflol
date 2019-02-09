abbcbefd38c06a7ea0fdf45b9a6542d3db3a048a
ProgrammerDAWE <35971005+ProgrammerDAWE@users.noreply.github.com>
2019-02-09 23:10:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ Client.on("message", (message)=>{
             let repMessagetoAdmins = new Discord.RichEmbed()
             .setDescription("❗ Report message ❗")
             .setColor("#d60c0c")
-            .setAuthor(repAuthor, [repAuthor.displayAvatarURL])
+            .setAuthor(repAuthor, repAuthor.avatarURL)
             .setThumbnail(repUser.displayAvatarURL)
             .addField("Nahlášení podal:", repAuthor + " (ID: " + repAuthorID + ")")
             .addField("Nahlášený uživatel:", repUser + " (ID: " + repUser.id + ")")
